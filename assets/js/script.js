ed0d4b08accadfb9688ada05e7944387c0542599
bencha27 <115957643+bencha27@users.noreply.github.com>
2022-12-30 16:08:48 -0700

    Added quiz functionality in JS (start and stop timer; save, display, and clear scores)

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,39 +13,65 @@ var choiceD = document.getElementById("choice-d");
 var correct = document.getElementById("correct");
 var incorrect = document.getElementById("incorrect");
 var nextButton = document.getElementById("next-button");
+var timeDisplay = document.getElementById("timer");
+var endButton = document.getElementById("end-button");
+var endEl = document.getElementById("end-content");
+var scoreDisplay = document.getElementById("score");
+var nameInput = document.getElementById("name-input")
+var submitButton = document.getElementById("submit-button")
+var homeButton = document.getElementById("home-button");
 var scoresEl = document.getElementById("scores-content");
+var scoresList = document.getElementById("scores-list");
+var clearButton = document.getElementById("clear-button");
 
 // Variables
 var number = 1;
-var questionIndexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var questionIndexArray = [];
 var choiceIndexArray = [];
-
-// Event listeners
-startButton.addEventListener("click", startQuiz);
-nextButton.addEventListener("click", nextQuestion);
-scoresButton.addEventListener("click", showScores);
+var timeLeft = 90;
+var score = 0;
+var savedScores = [];
 
 // startQuiz function to begin quiz
+startButton.addEventListener("click", startQuiz);
 function startQuiz() {
+    number = 1;
+    timeLeft = 90;
+    questionIndexArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
     displayQuestion();
 }
 
 // displayQuestion function to display question
 function displayQuestion() {
     // Hide intro content
+    homeButton.className = "hidden";
     introEl.className = "hidden";
+
+    // Start quiz timer
+    timeDisplay.textContent = timeLeft;
+    var timer = setInterval(displayTime, 1000);
+    function displayTime() {
+        timeLeft--;
+        timeDisplay.textContent = timeLeft;
+
+        if (timeLeft = 0) {
+            endQuiz();
+        }
+    }
 
     // Show question content
     questionEl.removeAttribute("class");
     questionNum.textContent = "Question " + number;
 
     // Choose and show question at random
-    choiceIndexArray = [0, 1, 2, 3];
-    var randomQuestion = Math.floor(Math.random() * questionIndexArray.length);
-    var currentQuestion = quizQuestions[randomQuestion];
+    var randomIndex = Math.floor(Math.random() * questionIndexArray.length);
+    var currentQuestionNum = questionIndexArray[randomIndex];
+    var currentQuestion = quizQuestions[currentQuestionNum];
     question.textContent = currentQuestion.question;
+    questionIndexArray.splice(randomIndex, 1);
 
     // Choose and show choices at random
+    choiceIndexArray = [0, 1, 2, 3];
     var currentChoices = currentQuestion.choices;
     var randomChoice = Math.floor(Math.random() * choiceIndexArray.length);
     var choiceNum = choiceIndexArray[randomChoice];
@@ -67,47 +93,119 @@ function displayQuestion() {
     choiceD.textContent = currentChoices[choiceNum];
     choiceIndexArray.splice(randomChoice, 1);
 
-    questionIndexArray.splice(randomQuestion, 1);
-
-    console.log(currentQuestion.question);
-    console.log(questionIndexArray);
-
     // Event listener for choices
     choicesEl.addEventListener("click", checkAnswer);
 
     // checkAnswer function to check user's choice
     function checkAnswer(event) {
-        // Display "Correct"/"Incorrect" based on user choice
+        // Pause timer
+        clearInterval(timer);
+
+        // Validate user choice
         var userChoice = event.target;
         if (userChoice.textContent === currentQuestion.answer) {
             correct.removeAttribute("class");
+            score++;
         } else {
             incorrect.removeAttribute("class");
+            if (timeLeft > 10) {
+                timeLeft -= 10;
+            } else {
+                timeLeft = 0;
+            }
         }
     
-        // Display "Next Question" button
-        nextButton.removeAttribute("class");
+        // Display "Next Question"/"End Quiz" button
+        if (number === 10) {
+            nextButton.className = "hidden";
+        } else {
+            nextButton.removeAttribute("class");
+        }
     
         // Prevent user from clicking choices multiple times
         choicesEl.removeEventListener("click", checkAnswer);
     }
+
+    // endQuiz function to display end content
+    endButton.addEventListener("click", endQuiz);
+    function endQuiz() {
+        clearInterval(timer);
+        currentQuestion = "";
+        questionEl.className = "hidden";
+        correct.className = "hidden";
+        incorrect.className = "hidden";
+        endEl.removeAttribute("class");
+        if (timeLeft = 0) {
+            endEl.firstElementChild.textContent = "Time's up!";
+        } else {
+            endEl.firstElementChild.textContent = "Finished!";
+        }
+        scoreDisplay.textContent = score;
+        homeButton.removeAttribute("class");
+    }
 }
 
-
 // nextQuestion function to display next question
+nextButton.addEventListener("click", nextQuestion);
 function nextQuestion() {
     number++;
     nextButton.className = "hidden";
     correct.className = "hidden";
     incorrect.className = "hidden";
-    // choiceIndex = [0, 1, 2, 3];
     displayQuestion();
-    // choicesEl.addEventListener("click", checkAnswer);
 }
 
-// showScores function to display scores
-function showScores() {
+// logScore function to save score
+submitButton.addEventListener("click", logScore);
+function logScore() {
+    var playerResult = {
+        "name": nameInput.value,
+        "score": score
+    }
+    storedScores = JSON.parse(localStorage.getItem("bioQuizScores"));
+    if (storedScores !== null) {
+        savedScores = storedScores;
+    }
+    savedScores.push(playerResult);
+    localStorage.setItem("bioQuizScores", JSON.stringify(savedScores));
+    nameInput.value = "";
+    showScores();
+}
 
+// showScores function to display saved scores
+scoresButton.addEventListener("click", showScores);
+function showScores() {
+    introEl.className = "hidden";
+    endEl.className = "hidden";
+    scoresEl.removeAttribute("class");
+    homeButton.removeAttribute("class");
+    scoresList.textContent = "";
+
+    var savedResults = JSON.parse(localStorage.getItem("bioQuizScores"));
+    if (savedResults !== null) {
+        for (i = 0; i < savedResults.length; i++) {
+            var resultLi = document.createElement("li");
+            var result = savedResults[i];
+            resultLi.textContent = result.name + ": " + result.score;
+            scoresList.appendChild(resultLi);
+        }
+    }
+}
+
+// clearScores function to clear saved scores
+clearButton.addEventListener("click", clearScores);
+function clearScores() {
+    localStorage.removeItem("bioQuizScores");
+    showScores();
+}
+
+// home function to return to home display
+homeButton.addEventListener("click", home);
+function home() {
+    introEl.removeAttribute("class");
+    questionEl.className = "hidden";
+    endEl.className = "hidden";
+    scoresEl.className = "hidden";
 }
 
 // Question bank
